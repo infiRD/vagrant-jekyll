55fbaf54fbee99e90ba30ec98615b1935fd72808
Martin Zamba <martin.zamba@gmail.com>
2017-02-18 03:47:25 +0100

    updated bootstrap file to install all required stuff

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -3,6 +3,39 @@
 # make sure no apt tasks are running in the background
 sudo ./waitForApt.sh
 
-# install whatever: 
+# ===================================================================================
+# install ruby: 
+# according to instructions on stackoverflow:
+# http://stackoverflow.com/questions/9056008/installed-ruby-1-9-3-with-rvm-but-command-line-doesnt-show-ruby-v?answertab=votes#tab-top
+
+echo "Removing old versions of ruby..."
+sudo apt-get --purge -y remove ruby-rvm ruby
+sudo rm -rf /usr/share/ruby-rvm /etc/rvmrc /etc/profile.d/rvm.sh
+
+echo "Installing latest version of ruby..."
+curl -L https://get.rvm.io | bash -s stable --ruby --autolibs=enable --auto-dotfiles
+
+
+# ===================================================================================
+# install nodeJS: 
+# refer to:
+# http://stackoverflow.com/questions/12913141/message-failed-to-fetch-from-registry-while-trying-to-install-any-module
+# https://github.com/nodesource/distributions
+
+echo "Removing old versions of nodeJS..."
+sudo apt-get purge -y nodejs npm
+
+echo "Installing latest version of nodeJS..."
+sudo apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+# ===================================================================================
+# install jekyll stuff:
+
+gem install github-pages therubyracer bundler jekyll-livereload --no-ri --no-rdoc
+
+
 
 
